1d0bcf46d11760fc8f86456259b6467d27173bb9
Vincent Costa <costa.vincent132@gmail.com>
2019-11-13 22:22:18 -0800

    Removed unecessary userData check in index.js

--- app/renderer/index.js
+++ app/renderer/index.js
@@ -4,14 +4,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import storage from "electron-json-storage"
-
-import fs from "fs"
-
-if(!fs.existsSync(storage.getDataPath() + "\\userData.json")) {
-    storage.set("userData", {})
-}
-
 ReactDOM.render(<App />, document.getElementById('app'));
 
 // If you want your app to work offline and load faster, you can change
