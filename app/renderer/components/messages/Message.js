4bd11f739ef786f5678c1be7dfa3beb657338199
Vincent Costa <costa.vincent132@gmail.com>
2020-02-02 01:53:41 -0800

    Upload files and download files

--- app/renderer/components/messages/Message.js
+++ app/renderer/components/messages/Message.js
@@ -1,5 +1,8 @@
 import React from "react"
 
+import { Button } from "@material-ui/core"
+import { IoMdDownload } from "react-icons/io"
+
 const Message = props => {
     const isMe = props.myself === props.parsed.sender
     let lastWasSame = false
@@ -23,16 +26,70 @@ const Message = props => {
         time = new Date(props.data.Timestamp)
     }
 
+    let extension = ""
+
+    if(props.parsed.type !== "MESSAGE") {
+        const reverse = props.parsed.content.name.split("").reverse().join("")
+
+        for(let i of reverse) {
+            extension += i
+
+            if(i === ".") {
+                break
+            }
+        }
+
+        extension = extension.split("").reverse().join("")
+    }
+
     return (
         <>
             { time && <p style = {{ textAlign: "center", margin: 0, color: props.backgroundText }}>{formatTime(time)}</p> }
 
-            <div style = {{ alignSelf: aligned, margin: "5px 10px", maxWidth: "50%", wordBreak: "break-all" }}>
+            <div 
+                style = {{ 
+                    alignSelf: aligned, 
+                    margin: "5px 10px", 
+                    maxWidth: "50%", 
+                    wordBreak: "break-all",
+                    border: props.parsed.type !== "MESSAGE" ? "1px solid " + color : "",
+                    borderRadius: border
+                }}
+            >
                 { !lastWasSame && <h4 style = {{ textAlign: textAlign, margin: 0, color: props.backgroundText }}>{sender}</h4> }
+                
                 <div style = {{ backgroundColor: color, minWidth: 20, padding: 10, borderRadius: border }}>
-                    <p style = {{ margin: 0, color: textColor }}>{props.parsed.content}</p>
-                    <p style = {{ margin: 0, fontSize: "0.8rem" }}>{formatMessageTime(props.data.Timestamp)}</p>
+                    { 
+                        props.parsed.type === "MESSAGE" ?  
+                        <p style = {{ margin: 0, color: textColor }}>{props.parsed.content}</p> : 
+                        <>
+                            <p style = {{ margin: 0, color: textColor }}>Short name: {props.parsed.content.name.slice(0, 25)}</p>
+                            <p style = {{ margin: "5px 0 0 0", color: textColor }}>Type: {props.parsed.content.extension}: "{extension}"</p>
+                            <p style = {{ margin: "5px 0 5px 0", color: textColor }}>Size: {(props.parsed.content.size / 1000000).toFixed(2)} MB</p>
+                        </>
+                    }
+                    
+                    <p style = {{ margin: 0, fontSize: "0.8rem", color: textColor }}>{formatMessageTime(props.data.Timestamp)}</p>
                 </div>
+
+                { 
+                    props.parsed.type !== "MESSAGE" && 
+                    <div style = {{ display: "flex", margin: "5px 10px", flexDirection: "column", alignItems: "flex-end" }}>
+                        <h6 style = {{ margin: 0, color: props.backgroundText }}>{props.parsed.content.name}</h6> 
+                        { 
+                            props.parsed.type !== "MESSAGE" && 
+                            <Button 
+                                color = "primary"
+                                variant = "contained"
+                                onClick = {_ => props.fileDownloaded(props.parsed.content)} 
+                                style = {{ margin: "10px 0px 10px 10px", color: textColor, width: 150 }}
+                            >
+                                Download 
+                                <IoMdDownload size = {23} style = {{ marginLeft: 5 }} />
+                            </Button> 
+                        }
+                    </div>
+                }
             </div>
         </>
     )
